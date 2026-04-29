27e0a2396c087840342e70d42679ee0c7c36c844
Abel Gabor <abel@abel.hu>
2026-04-29 22:44:32 +0200

    chore: baquatelle.pythonanywhere.com is added to ALLOWED_HOSTS in settings.py

--- wup/settings.py
+++ wup/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*v@_5lwtxo&^z1iu^cs)z76de@b7r9xj6td8d8fl55%^z*21y!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['baquatelle.pythonanywhere.com']
 
 
 # Application definition
